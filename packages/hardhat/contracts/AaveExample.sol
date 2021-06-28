2113fc6c3e7bbf0cb072a6c0461db836d05a3d11
Jorge Orozco <jorge.orosan@gmail.com>
2021-06-27 21:21:43 -0600

    organisation creation with aave percentage

--- packages/hardhat/contracts/AaveExample.sol
+++ packages/hardhat/contracts/AaveExample.sol
@@ -1,147 +1,84 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
-interface IERC20 {
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `recipient`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     * 
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `sender` to `recipient` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-//no need to import safemath in solidity 8
-
-/*
-interface IaToken {
-    function balanceOf(address _user) external view returns (uint256);
-    function redeem(uint256 _amount) external;
-}
-*/
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
 interface IaveProvider{
+
     function getLendingPool() external view returns (address);
+
 }
+
 interface IAaveLendingPool {
+
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-    
+
     function withdraw(address asset, uint256 amount, address to) external;
+
 }
 
 contract AaveExample {
 
-    uint public balanceReceived = 0;
-
-    IERC20 public usdc = IERC20(0x13512979ADE267AB5100878E2e0f485B568328a4); //Kovan address     USDC address 0xe22da380ee6B445bb8273C81944ADEB6E8450422
-    IERC20 public aUsdc = IERC20(0xFF3c8bc103682FA918c954E84F5056aB4DD5189d); //Kovan address USDC
-    IaveProvider provider   = IaveProvider(0x88757f2f99175387aB4C6a4b3067c77A695b0349);
-    IAaveLendingPool public aaveLendingPool = IAaveLendingPool(provider.getLendingPool()); // Kovan address
-
-    uint256 max = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-
-    mapping(address => uint256) public userDepositedUsdc;
-
-    constructor()   {
-        usdc.approve(address(aaveLendingPool), max);
-        aUsdc.approve(address(aaveLendingPool), max);
+    struct Organisation {
+        uint256 aavePercentage;
+        uint256 aaveAmount;
+        bool created;
     }
 
-    function approveUnlimitedUsdc() public{
-        usdc.approve(address(this), max);
+    IERC20 private usdt = IERC20(0x13512979ADE267AB5100878E2e0f485B568328a4);
+    IERC20 private aUsdt = IERC20(0xFF3c8bc103682FA918c954E84F5056aB4DD5189d);
+    IaveProvider private provider = IaveProvider(0x88757f2f99175387aB4C6a4b3067c77A695b0349);
+    IAaveLendingPool private aaveLendingPool = IAaveLendingPool(provider.getLendingPool());
+    mapping(address => uint256) public users;
+    mapping(address => Organisation) public organisations;
+
+    event organisationCreated(address organisation, uint256 aavePercentage);
+    event userDeposit(address user, address organisation, uint256 amount);
+    event organisationWithdrawal(address organisation, uint256 amount);
+
+    constructor() {
+        usdt.approve(address(aaveLendingPool), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+        aUsdt.approve(address(aaveLendingPool), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
     }
 
-    function receiveMoney() public payable {
-        balanceReceived += msg.value;
+    /// @notice function to create an organisation with its configuration. Emits an organisationCreated event.
+    /// @param aavePercentage organisation decides to deposit into aave
+    function createOrganisation (uint256 aavePercentage) public {
+        require(aavePercentage <= 100, "Percentage can't be greather than 100");
+        organisations[msg.sender].aavePercentage = aavePercentage;
+        organisations[msg.sender].created = true;
+        emit organisationCreated(msg.sender, aavePercentage);
     }
 
-    function userDepositUsdc(uint256 _amountInUsdc) external {
-        userDepositedUsdc[msg.sender] += _amountInUsdc;
-        require(usdc.transferFrom(msg.sender, address(this), _amountInUsdc), "USDC Transfer failed!");
-        aaveLendingPool.deposit(address(usdc), _amountInUsdc, address(this), 0);
+    /// @notice function to deposit into aave organisation address
+    /// @param _usdt amount to deposit to organisation
+    /// @param _organisation address to deposit
+    function deposit(uint256 _usdt, address _organisation) public {
+        require(organisations[_organisation].created, "Organisation doesn't exist");
+        require(usdt.transferFrom(msg.sender, address(this), _usdt), "USDT Transfer failed!");
+        users[msg.sender] += _usdt;
+        organisations[_organisation].aaveAmount += _usdt;
+        aaveLendingPool.deposit(address(usdt), _usdt, _organisation, 0);
+        emit userDeposit(msg.sender, _organisation, _usdt);
     }
 
-    function userBalanceUsdc(address sender) external view returns (uint256) {
-        return aUsdc.balanceOf(sender);
+    /// @notice function to get balance of an organisation address
+    /// @param _organisation to find balance
+    function organisationBalance(address _organisation) public view returns (uint256) {
+        require(organisations[_organisation].created, "Organisation doesn't exist");
+        return aUsdt.balanceOf(_organisation);
     }
 
-    function userInterests(address sender) external view returns (uint256) {
-        return this.userBalanceUsdc(sender) - userDepositedUsdc[msg.sender];
+    /// @notice function that allows an organisation to withdraw an amount from aave.
+    /// In this moment, it doesn't work because smart contract doesn't have 'permit' approval from organisation
+    /// If smart contract has 'permit' approval from organisation, organisation can 'withdraw' directly from aave. In that case, function could be unnecessary.
+    /// @param amount to withdraw
+    function withdraw(uint256 amount) public {
+        require(organisations[msg.sender].created, "Organisation doesn't exist");
+        require(organisations[msg.sender].aaveAmount >= amount, "Insufficient funds");
+        organisations[msg.sender].aaveAmount -= amount;
+        aaveLendingPool.withdraw(address(usdt), amount, msg.sender);
+        emit organisationWithdrawal(msg.sender, amount);
     }
 
-    function userWithdrawUsdc(uint256 _amountInUsdc) external {
-        require(this.userInterests(msg.sender) >= _amountInUsdc, "You cannot withdraw more than deposited!");
-        userDepositedUsdc[msg.sender] = userDepositedUsdc[msg.sender] - _amountInUsdc;
-        aaveLendingPool.withdraw(address(usdc), _amountInUsdc, msg.sender);
-    }
 }
