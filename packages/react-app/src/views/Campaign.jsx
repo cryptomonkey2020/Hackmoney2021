dcf08912c4a3f682a40524f7f1a847373964160a
zhenyangg <chrenelias@outlook.com>
2021-07-03 16:25:04 +0800

    Changed last chart column date

--- packages/react-app/src/views/Campaign.jsx
+++ packages/react-app/src/views/Campaign.jsx
@@ -710,7 +710,7 @@ const dummyCampaignInfo = {
             income: 301.2
         },
         {
-            date: new Date(),
+            date: '2021-07-03T00:00:00+00:00',
             amount: 88368,
             income: 334.6
         }
